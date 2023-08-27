a0a5ad57c870b98a48a59e255ca9fcf8a9e707fa
Svirin Evgeny <esvirin@mail.com>
2023-08-27 12:23:28 +0200

    envs

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
+  const payload = fetch("https://jsonplaceholder.typicode.com/todos").then(
+    (res) => res.json()
+  );
+  console.log(payload);
   return <div>Main Page</div>;
 }
